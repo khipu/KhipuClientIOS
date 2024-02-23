b29e26f7029ec6a8870b2836293f3a45dd0300e1
Juan Horta <juan.horta@khipu.com>
2024-02-23 18:04:24 -0300

    add warningmessage

---
+++ khenshinClientIos/Classes/Messages/WarningMessage.swift
@@ -0,0 +1,180 @@
+import UIKit
+import KhenshinProtocol
+
+class WarningMessage: UIView {
+
+    var operationWarning: OperationWarning
+    private let title: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = UIColor.black
+        label.text = "Pago en verificación"
+        return label
+    }()
+
+    private let subTitle: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = UIColor.black
+        return label
+    }()
+
+
+    private let body: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor(red: 252/255, green: 248/255, blue: 227/255, alpha: 1.0) // #fcf8e3
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10 
+        return label
+    }()
+
+    private let monto: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.black
+        label.text = "Monto"
+        return label
+    }()
+
+    private let comercio: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.black
+        label.text = "Comercio"
+        return label
+    }()
+
+    private let operacion: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.black
+        label.text = "Operación"
+        return label
+    }()
+
+    private let montoValue: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.gray
+        return label
+    }()
+
+    private let comercioValue: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.gray
+        return label
+    }()
+
+    private let operacionValue: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.gray
+        return label
+    }()
+
+    private lazy var contentContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+
+    private lazy var infoContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.alignment = .center
+        return stackView
+    }()
+
+    private let resultIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+
+    init(frame: CGRect, operationWarning: OperationWarning) {
+        self.operationWarning = operationWarning
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        //valores pago
+        subTitle.text = self.operationWarning.title
+        operacionValue.text = self.operationWarning.operationID
+        body.text = self.operationWarning.body
+
+        //valores faltantes
+        montoValue.text = "$1000"
+        comercioValue.text = "Comercio"
+
+        addSubview(contentContainer)
+        contentContainer.addArrangedSubview(resultIconImageView)
+
+        let montoStackView = UIStackView(arrangedSubviews: [monto, montoValue])
+        montoStackView.axis = .horizontal
+        montoStackView.spacing = 5
+        montoStackView.alignment = .center
+
+        let comercioStackView = UIStackView(arrangedSubviews: [comercio, comercioValue])
+        comercioStackView.axis = .horizontal
+        comercioStackView.spacing = 5
+        comercioStackView.alignment = .center
+
+        let operacionStackView = UIStackView(arrangedSubviews: [operacion, operacionValue])
+        operacionStackView.axis = .horizontal
+        operacionStackView.spacing = 5
+        operacionStackView.alignment = .center
+
+        infoContainer.addArrangedSubview(montoStackView)
+        infoContainer.addArrangedSubview(comercioStackView)
+        infoContainer.addArrangedSubview(operacionStackView)
+
+        contentContainer.addArrangedSubview(infoContainer)
+
+        contentContainer.backgroundColor = UIColor.white
+        infoContainer.backgroundColor = UIColor.white
+
+        addSubview(title)
+        addSubview(subTitle)
+        addSubview(body)
+        addSubview(contentContainer)
+
+        resultIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        title.translatesAutoresizingMaskIntoConstraints = false
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        contentContainer.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            resultIconImageView.leadingAnchor.constraint(equalTo: contentContainer.leadingAnchor, constant: 16),
+            resultIconImageView.trailingAnchor.constraint(equalTo: contentContainer.trailingAnchor, constant: -16),
+            resultIconImageView.heightAnchor.constraint(equalToConstant: 50),
+
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            title.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            subTitle.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 10),
+            subTitle.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            contentContainer.topAnchor.constraint(equalTo: subTitle.bottomAnchor, constant: 20),
+            contentContainer.centerXAnchor.constraint(equalTo: centerXAnchor),
+            contentContainer.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -16),
+        ])
+    }
+}
