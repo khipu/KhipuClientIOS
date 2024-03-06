baf2d2ac566612145253727122b8d09f867db6b9
Juan Horta <juan.horta@khipu.com>
2024-03-06 11:02:10 -0300

    incorporar hint y placeholder email, rut y textfield

--- Example/khenshinClientIos/ViewController.swift
+++ Example/khenshinClientIos/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     lazy private var sampleInput: UITextField = {
         let sampleTextField =  UITextField()
 
-        sampleTextField.text = "nsmczkfaoqzv"
+        sampleTextField.text = "hcwaqoakp0up"
         sampleTextField.font = UIFont.systemFont(ofSize: 15)
         sampleTextField.borderStyle = UITextField.BorderStyle.roundedRect
         sampleTextField.autocorrectionType = UITextAutocorrectionType.no
--- khenshinClientIos/Classes/Fields/RutField.swift
+++ khenshinClientIos/Classes/Fields/RutField.swift
@@ -3,16 +3,13 @@ import KhenshinProtocol
 
 class RutField: BaseField, UITextFieldDelegate {
     
-    lazy private var error = ComponentBuilder.buildLabel(textColor: .red, fontSize: 12, backgroundColor: .black)
+    lazy private var error = ComponentBuilder.buildLabel(textColor: .red, fontSize: 9, backgroundColor: .black)
     lazy private var input = ComponentBuilder.buildCustomTextField(font: UIFont.systemFont(ofSize: 14), borderStyle: .roundedRect)
+    lazy private var hint  = ComponentBuilder.buildLabel(textColor: .lightGray, fontSize: 9, backgroundColor: UIColor.white)
 
     
     required init?(formItem: FormItem) {
         super.init(formItem: formItem)
-    }
-    
-    override func getValue() -> String {
-        return input.text!
     }
 
     required init?(coder: NSCoder) {
@@ -20,26 +17,41 @@ class RutField: BaseField, UITextFieldDelegate {
     }
 
     override func setupUI() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        self.addGestureRecognizer(tapGesture)
-        input.placeholder = self.formItem!.label
+        configureGestures()
+        configureInputField()
+        
         addSubview(input)
+        addSubview(hint)
         addSubview(error)
+        input.translatesAutoresizingMaskIntoConstraints = false
+        hint.translatesAutoresizingMaskIntoConstraints = false
+        error.translatesAutoresizingMaskIntoConstraints = false
         translatesAutoresizingMaskIntoConstraints = false
-        input.translatesAutoresizingMaskIntoConstraints = false
-        error.translatesAutoresizingMaskIntoConstraints = false
 
-        input.translatesAutoresizingMaskIntoConstraints = false
-        error.translatesAutoresizingMaskIntoConstraints = false
-
+        configureLengthConstraints()
+        
         NSLayoutConstraint.activate([
             input.topAnchor.constraint(equalTo: self.topAnchor),
             input.widthAnchor.constraint(equalTo: self.widthAnchor),
-            error.topAnchor.constraint(equalTo: input.bottomAnchor),
-            error.leadingAnchor.constraint(equalTo: leadingAnchor),
-            error.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            hint.topAnchor.constraint(equalTo: input.bottomAnchor),
+            hint.trailingAnchor.constraint(equalTo: input.trailingAnchor),
+             
+            error.topAnchor.constraint(equalTo: hint.bottomAnchor),
+            error.trailingAnchor.constraint(equalTo: input.trailingAnchor),
             error.bottomAnchor.constraint(equalTo: bottomAnchor),
+
         ])
+    }
+    
+    private func configureGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.addGestureRecognizer(tapGesture)
+    }
+
+    private func configureInputField() {
+        input.placeholder = self.formItem!.label
+        hint.text = self.formItem?.hint
     }
 
     override func validate() -> Bool {
@@ -55,6 +67,10 @@ class RutField: BaseField, UITextFieldDelegate {
             error.text = ""
             return true
         }
+    }
+    
+    override func getValue() -> String {
+        return input.text!
     }
 
     private func validateRut(_ rut: String) -> Bool {
@@ -80,6 +96,28 @@ class RutField: BaseField, UITextFieldDelegate {
         }
 
         return S != 0 ? "\(S - 1)" : "k"
+    }
+    
+    private func configureLengthConstraints() {
+        if let maxLength = self.formItem?.maxLength, maxLength > 0 {
+            input.addTarget(self, action: #selector(limitLength), for: .editingChanged)
+        }
+    }
+
+    @objc private func limitLength() {
+        guard let text = input.text else { return }
+
+        if let maxLength = self.formItem?.maxLength, text.count > Int(maxLength) {
+            let index = text.index(text.startIndex, offsetBy: min(text.count, Int(maxLength)))
+            input.text = String(text.prefix(upTo: index))
+        }
+    }
+
+    func textField(_ input: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = input.text ?? ""
+        let prospectiveText = (currentText as NSString).replacingCharacters(in: range, with: string)
+
+        return (self.formItem?.maxLength.map { prospectiveText.count <= Int($0) } ?? true)
     }
     
     @objc private func handleTap() {
