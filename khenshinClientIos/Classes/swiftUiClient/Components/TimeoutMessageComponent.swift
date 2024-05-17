d0852975f940d54d3f80e32b1390370f5973bb5e
mauriciocastillo <mauricio.castillo@khipu.com>
2024-05-17 09:19:52 -0400

    correccion timeout component por incompatibilidad con xcode antiguo

--- khenshinClientIos/Classes/swiftUiClient/Components/TimeoutMessageComponent.swift
+++ khenshinClientIos/Classes/swiftUiClient/Components/TimeoutMessageComponent.swift
@@ -9,36 +9,48 @@ struct TimeoutMessageComponent: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: Dimens.verySmall) {
-            Text(viewModel.uiState.translator.t("page.timeout.session.closed"))
-                .foregroundColor(Color(.label))
-                .font(.title2)
-                .multilineTextAlignment(.center)
-            Spacer().frame(height: Dimens.extraSmall)
-            FormWarning(text: viewModel.uiState.translator.t("page.timeout.try.again"))
-            Spacer().frame(height: Dimens.extraLarge)
+            Group {
+                Text(viewModel.uiState.translator.t("page.timeout.session.closed"))
+                    .foregroundColor(Color(.label))
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                    .frame(height: Dimens.extraSmall)
+            }
+            Group {
+                FormWarning(text: viewModel.uiState.translator.t("page.timeout.try.again"))
+                Spacer()
+                    .frame(height: Dimens.extraLarge)
+            }
+            Group {
+                Image(systemName: "clock.arrow.circlepath")
+                    .font(.system(size: Dimens.huge))
+                    .foregroundColor(Color.gray.opacity(0.7))
+                
+                Text(viewModel.uiState.translator.t("page.timeout.end"))
+                    .foregroundColor(Color(.label))
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                    .frame(height: Dimens.extraLarge)
+            }
             
-            
-            Image(systemName: "clock.arrow.circlepath")
-                .font(.system(size: Dimens.huge))
-                .foregroundColor(Color.gray.opacity(0.7))
-
-            
-            Text(viewModel.uiState.translator.t("page.timeout.end"))
-                .foregroundColor(Color(.label))
-                .font(.footnote)
-                .multilineTextAlignment(.center)
-            
-            Spacer().frame(height: Dimens.extraLarge)
-
-            Text(viewModel.uiState.translator.t("default.operation.code.label"))
-                .foregroundColor(Color(.label))
-                .font(.footnote)
-                .multilineTextAlignment(.center)
-            
-            CopyToClipboardOperationId(text: operationFailure.operationID ?? "", textToCopy: FieldUtils.formatOperationId(operationId: operationFailure.operationID ?? ""), background:Color(red: 60/255, green: 180/255, blue: 229/255))
-            
-            Spacer()
-            
+            Group {
+                Text(viewModel.uiState.translator.t("default.operation.code.label"))
+                    .foregroundColor(Color(.label))
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                CopyToClipboardOperationId(
+                    text: operationFailure.operationID ?? "",
+                    textToCopy: FieldUtils.formatOperationId(operationId: operationFailure.operationID ?? ""),
+                    background:Color(red: 60/255, green: 180/255, blue: 229/255)
+                )
+                
+                Spacer()
+                
+            }
+                
             MainButton(
                 text: viewModel.uiState.translator.t("page.redirectManual.redirecting"),
                 enabled: true,
