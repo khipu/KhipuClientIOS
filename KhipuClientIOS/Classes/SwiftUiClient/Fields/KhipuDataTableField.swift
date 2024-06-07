29bd67482f3c1e16707aeb42cfe07ab7d478c68d
Belén Fuentes <bnfuentesm@gmail.com>
2024-06-07 15:01:13 -0400

    SLCA-266 - Completes DataTable

--- KhipuClientIOS/Classes/SwiftUiClient/Fields/KhipuDataTableField.swift
+++ KhipuClientIOS/Classes/SwiftUiClient/Fields/KhipuDataTableField.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 import KhenshinProtocol
 
-@available(iOS 13.0, *)
+@available(iOS 15.0, *)
 struct KhipuDataTableField: View {
     var formItem: FormItem
     var hasNextField: Bool
@@ -11,7 +11,79 @@ struct KhipuDataTableField: View {
     @State var textFieldValue: String = ""
     
     var body: some View {
-        Text(formItem.label ?? "")
-        TextField(formItem.placeHolder ?? "", text: $textFieldValue)
+        KhipuDataTable(dataTable: formItem.dataTable!)
+    }
+}
+
+@available(iOS 15.0, *)
+struct KhipuDataTableField_Previews: PreviewProvider {
+    static var previews: some View {
+        let isValid: (Bool) -> Void = { param in }
+        let returnValue: (String) -> Void = { param in }
+        let formItem1 = try! FormItem(
+         """
+           {
+            "id": "item1",
+            "label": "item1",
+            "type": "\(FormItemTypes.dataTable.rawValue)",
+            "dataTable": {"rows":[], "rowSeparator":{}}
+           }
+         """
+        )
+        let formItem2 = try! FormItem(
+         """
+           {
+            "id": "item1",
+            "label": "item1",
+            "type": "\(FormItemTypes.dataTable.rawValue)",
+            "dataTable": {"rows":[{"cells":[{"text":"Cell 1"}]}], "rowSeparator":{}}
+           }
+         """
+        )
+        
+        let formItem3 = try! FormItem(
+        """
+            {
+            "id": "item1",
+            "label": "item1",
+            "type":"\(FormItemTypes.dataTable.rawValue)",
+            "dataTable": {
+                "rows":[
+                    {"cells":[
+                        {"text":"Cell 1"},
+                        {"text":"Cell 2"},
+                        {"text":"Cell 3"}
+                    ]},
+                    {"cells":[
+                        {"text":"Cell 1"},
+                        {"text":"Cell 2"}
+                    ]}
+                ], "rowSeparator":{}}
+            }
+        """
+        )
+        return VStack {
+            Text("DataTable empty:")
+            KhipuDataTableField(
+                formItem: formItem1,
+                hasNextField: false,
+                isValid: isValid,
+                returnValue: returnValue
+            )
+            Text("DataTable one cell:")
+            KhipuDataTableField(
+                formItem: formItem2,
+                hasNextField: false,
+                isValid: isValid,
+                returnValue: returnValue
+            )
+            Text("DataTable two rows:")
+            KhipuDataTableField(
+                formItem: formItem3,
+                hasNextField: false,
+                isValid: isValid,
+                returnValue: returnValue
+            )
+        }
     }
 }
