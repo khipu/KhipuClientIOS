5baafd903ad7d423150703c50c818b66a788c2e5
Alejandro Vera <alejo@Alejandros-MacBook-Pro.local>
2024-06-12 18:18:37 -0400

    SLCA-284 Switch

--- KhipuClientIOS/Classes/SwiftUiClient/Fields/KhipuSwitchField.swift
+++ KhipuClientIOS/Classes/SwiftUiClient/Fields/KhipuSwitchField.swift
@@ -1,17 +1,113 @@
 import SwiftUI
 import KhenshinProtocol
 
-@available(iOS 13.0, *)
+@available(iOS 15.0, *)
 struct KhipuSwitchField: View {
     var formItem: FormItem
     var hasNextField: Bool
     var isValid: (Bool) -> Void
     var returnValue: (String) -> Void
-    @State var passwordVisible: Bool = false
-    @State var textFieldValue: String = ""
+    @ObservedObject var viewModel: KhipuViewModel
+    @State var currentTime: TimeInterval = Date().timeIntervalSince1970
+    @State var lastModificationTime: TimeInterval = 0
+    @State var error: String = ""
+    @State var isChecked: Bool = false
     
     var body: some View {
-        Text(formItem.label ?? "")
-        TextField(formItem.placeHolder ?? "", text: $textFieldValue)
+        VStack(alignment: .leading, spacing:0) {
+            HStack {
+                Toggle(isOn: $isChecked) {
+                    Text(formItem.label ?? "")
+                }
+            .onAppear {
+                    isChecked = formItem.defaultState == "on"
+                }
+                .onChange(of: isChecked) { newValue in
+                    onChange(newValue: newValue)
+                }
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            if shouldDisplayError() {
+                ErrorLabel(text: error)
+            }
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            currentTime = Date().timeIntervalSince1970
+        }
+    }
+    
+    
+    func onChange(newValue: Bool) {
+        isChecked = newValue
+        error = ValidationUtils.valiateCheckRequiredState(isChecked,
+                                                          formItem.requiredState,
+                                                          viewModel.uiState.translator)
+        isValid(error.isEmpty)
+        returnValue(String(newValue))
+        lastModificationTime = Date().timeIntervalSince1970
+    }
+    
+    func shouldDisplayError() -> Bool {
+        return !error.isEmpty && (currentTime - lastModificationTime > 1)
+    }
+    
+}
+
+@available(iOS 15.0, *)
+struct KhipuSwitchField_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = KhipuViewModel()
+        let isValid: (Bool) -> Void = { param in }
+        let returnValue: (String) -> Void = { param in }
+        viewModel.uiState = KhipuUiState()
+        viewModel.uiState.translator = KhipuTranslator(translations: [:])
+        
+        let formItem1 = try! FormItem(
+                 """
+                     {
+                       "id": "item1",
+                       "label": "item1",
+                       "type": "\(FormItemTypes.formItemTypesSWITCH.rawValue)",
+                       "defaultState": "off"
+                     }
+                 """
+        )
+        let formItem2 = try! FormItem(
+                 """
+                     {
+                       "id": "item1",
+                       "label": "item1",
+                       "type": "\(FormItemTypes.formItemTypesSWITCH.rawValue)",
+                       "defaultState": "on",
+                       "requiredState": "on"
+                     }
+                 """
+        )
+        return VStack {
+            KhipuSwitchField(
+                formItem: formItem1,
+                hasNextField: false,
+                isValid:  isValid,
+                returnValue: returnValue,
+                viewModel: viewModel
+            )
+            KhipuSwitchField(
+                formItem: formItem2,
+                hasNextField: false,
+                isValid:  isValid,
+                returnValue: returnValue,
+                viewModel: viewModel
+            )
+        }
+        .environmentObject(ThemeManager())
     }
 }
