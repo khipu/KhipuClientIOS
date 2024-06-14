cd0923220fd30bff55f53c0758fd934606a26e35
Alejandro Vera <alejandro.vera@khipu.com>
2024-06-14 10:37:44 -0400

    SLCA-292 tests for switch/checkbox/headerCheckbox

---
+++ Example/Tests/Fields/CheckboxFieldTest.swift
@@ -0,0 +1,57 @@
+
+import XCTest
+import SwiftUI
+import ViewInspector
+import KhenshinProtocol
+
+@testable import KhipuClientIOS
+
+@available(iOS 15.0.0, *)
+final class CheckboxFieldTest: XCTestCase {
+    
+    func testCheckboxFieldView() throws {
+        let viewModel = KhipuViewModel()
+        let isValid: (Bool) -> Void = { param in }
+        let returnValue: (String) -> Void = { param in }
+        
+        viewModel.uiState = KhipuUiState()
+        viewModel.uiState.translator = KhipuTranslator(translations: [:])
+        
+        let formItem = try! FormItem(
+                 """
+                     {
+                       "id": "item",
+                       "label": "Some stuff",
+                       "type": "\(FormItemTypes.checkbox.rawValue)",
+                       "defaultState": "on"
+                     }
+                 """
+         )
+        
+        var view = KhipuCheckboxField(
+            formItem: formItem,
+            hasNextField: false,
+            isValid:  isValid,
+            returnValue: returnValue,
+            viewModel: viewModel
+        )
+       
+        let instected = try view.environmentObject(ThemeManager()).inspect()
+        
+        let label = try instected.find(viewWithAccessibilityIdentifier: "labelText")
+        let text = try label.text().string()
+        XCTAssertEqual(text, "Some stuff")
+        
+        let expectation = view.on(\.didAppear) { view in
+            let toggle = try view
+                .vStack()
+                .hStack(0)
+                .toggle(0)
+            XCTAssertTrue(try toggle.isOn())
+        }
+        
+        ViewHosting.host(view: view.environmentObject(ThemeManager()))
+        
+        wait(for: [expectation], timeout: 0.1)
+    }
+}
---
+++ Example/Tests/Fields/SwitchFieldTest.swift
@@ -0,0 +1,60 @@
+import XCTest
+import SwiftUI
+import ViewInspector
+import KhenshinProtocol
+
+@testable import KhipuClientIOS
+
+@available(iOS 15.0.0, *)
+final class SwitchFieldTest: XCTestCase {
+    
+    func testSwitchFieldView() throws {
+        let viewModel = KhipuViewModel()
+        let isValid: (Bool) -> Void = { param in }
+        let returnValue: (String) -> Void = { param in }
+        
+        viewModel.uiState = KhipuUiState()
+        viewModel.uiState.translator = KhipuTranslator(translations: [:])
+        
+        let formItem = try! FormItem(
+                 """
+                     {
+                       "id": "item",
+                       "label": "Accept some stuff",
+                       "hint": "You must accept",
+                       "type": "\(FormItemTypes.formItemTypesSWITCH.rawValue)",
+                       "defaultState": "on"
+                     }
+                 """
+        )
+        
+        var view = KhipuSwitchField(
+            formItem: formItem,
+            hasNextField: false,
+            isValid:  isValid,
+            returnValue: returnValue,
+            viewModel: viewModel
+        )
+        let instected = try view.environmentObject(ThemeManager()).inspect()
+        
+        let label = try instected.find(viewWithAccessibilityIdentifier: "labelText")
+        let text = try label.text().string()
+        XCTAssertEqual(text, "Accept some stuff")
+        
+        let hint = try instected.find(viewWithAccessibilityIdentifier: "hintText").text().string()
+        XCTAssertEqual(hint, "You must accept")
+        
+        
+        let expectation = view.on(\.didAppear) { view in
+            let toggle = try view
+                .vStack()
+                .hStack(0)
+                .toggle(0)
+            XCTAssertTrue(try toggle.isOn())
+        }
+        
+        ViewHosting.host(view: view.environmentObject(ThemeManager()))
+        
+        wait(for: [expectation], timeout: 0.1)
+    }
+}
