74e7397e651744f516bb42fffc02d3844dbdbd29
Alejandro Vera <alejandro.vera@khipu.com>
2024-06-19 16:18:49 -0400

    SLCA-311 Otp tests

--- Example/Tests/Fields/OtpFieldTest.swift
+++ Example/Tests/Fields/OtpFieldTest.swift
@@ -6,47 +6,44 @@ import KhenshinProtocol
 @testable import KhipuClientIOS
 
 @available(iOS 15.0.0, *)
-final class CoordinatesFieldTest: XCTestCase {
-    
-    func testCoordinatesFieldView() throws {
+final class OtpFieldTest: XCTestCase {
+
+    func testOtpFieldView() throws {
         let viewModel = KhipuViewModel()
         let isValid: (Bool) -> Void = { param in }
         let returnValue: (String) -> Void = { param in }
         
         viewModel.uiState = KhipuUiState()
         viewModel.uiState.translator = KhipuTranslator(translations: [:])
-        
-        let formItem = try! FormItem(
+
+          let formItem = try! FormItem(
                      """
                          {
                            "id": "item1",
-                           "labels": ["Coord 1", "Coord 2", "Coord 3"],
-                           "type": "\(FormItemTypes.coordinates.rawValue)",
+                           "label": "Please use your OTP",
+                           "length": 4,
+                           "type": "\(FormItemTypes.otp.rawValue)",
                            "hint": "Give me the answer",
                             "number": false,
                          }
                      """
-        )
-        
-        let view = CoordinatesField(
+            )
+      
+        let view = OtpField(
             formItem: formItem,
             isValid:  isValid,
             returnValue: returnValue
         )
         let inspected = try view.environmentObject(ThemeManager()).inspect()
         
+        let label = try inspected.find(viewWithAccessibilityIdentifier: "labelText")
+        let text = try label.text().string()
+        XCTAssertEqual(text, "Please use your OTP")
+        
         let hint = try inspected.find(viewWithAccessibilityIdentifier: "hintText").text().string()
         XCTAssertEqual(hint, "Give me the answer")
         
-        
-        for index in 0..<3 {
-            let label = try inspected.find(viewWithAccessibilityIdentifier: "coordinateItem\(index + 1)")
-            XCTAssertEqual(try label
-                .vStack()
-                .view(FieldLabel.self, 0)
-                .vStack(0)
-                .text(0)
-                .string(), "Coord \(index + 1)")
+        for index in 0..<4 {
             let coordInput = try inspected.find(viewWithAccessibilityIdentifier: "coordinateInput\(index + 1)")
             XCTAssertNoThrow(try coordInput
                 .group()
