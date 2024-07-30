82c9b964fd78b0c0ffeaeacb112256bf8492ed81
Emilio Davis <emilio.davis@khipu.com>
2024-07-30 15:45:51 -0400

    verySmall separation in FooterComponent

--- KhipuClientIOS/Classes/SwiftUiClient/Components/FooterComponent.swift
+++ KhipuClientIOS/Classes/SwiftUiClient/Components/FooterComponent.swift
@@ -8,12 +8,11 @@ struct FooterComponent: View {
     
     var body: some View {
         if showFooter{
-            HStack(alignment: .center, spacing: Dimens.Spacing.small) {
+            HStack(alignment: .center, spacing: Dimens.Spacing.verySmall) {
                 
                 Text(translator.t("footer.powered.by"))
                     .font(themeManager.selectedTheme.fonts.font(style: .semiBold, size: 12))
                     .foregroundColor(themeManager.selectedTheme.colors.onSurfaceVariant)
-                
                 AsyncImage(url: URL(string: "https://s3.amazonaws.com/static.khipu.com/icon/logo-khipu-color.png")) { image in
                     image
                         .resizable()
