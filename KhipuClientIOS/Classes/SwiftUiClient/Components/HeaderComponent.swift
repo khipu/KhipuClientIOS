fd86f307a32890ab24df372a1977dfee7f2122cd
Juan Horta <juan.horta@khipu.com>
2024-07-30 13:14:12 -0400

    header show image only valid url

--- KhipuClientIOS/Classes/SwiftUiClient/Components/HeaderComponent.swift
+++ KhipuClientIOS/Classes/SwiftUiClient/Components/HeaderComponent.swift
@@ -41,14 +41,16 @@ struct HeaderComponent: View {
     
     private var headerContent: some View {
         HStack() {
-            AsyncImage(url: URL(string: operationInfo?.merchant?.logo ?? "")) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:Dimens.Frame.slightlyLarger, height:Dimens.Frame.slightlyLarger)
-                    .clipShape(RoundedRectangle(cornerRadius: Dimens.CornerRadius.verySmall))
-            } placeholder: {
-                ProgressView()
+            if let logoURLString = operationInfo?.merchant?.logo, let logoURL = URL(string: logoURLString), UIApplication.shared.canOpenURL(logoURL) {
+                AsyncImage(url: logoURL) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: Dimens.Frame.slightlyLarger, height: Dimens.Frame.slightlyLarger)
+                        .clipShape(RoundedRectangle(cornerRadius: Dimens.CornerRadius.verySmall))
+                } placeholder: {
+                    ProgressView()
+                }
             }
             
             VStack(alignment: .leading, spacing:Dimens.Spacing.verySmall) {
