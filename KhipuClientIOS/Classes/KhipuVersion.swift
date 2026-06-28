f545c66e22c0aa09b4c72391d0d50e1abf76d90c
Anka <runner@iad20-eo1212-041d7ba7-6f72-4bee-a322-4ab02a9d7f4b-E260B3DF308E.local>
2026-06-28 23:12:20 +0000

    Version sync with podfile

--- KhipuClientIOS/Classes/KhipuVersion.swift
+++ KhipuClientIOS/Classes/KhipuVersion.swift
@@ -6,7 +6,7 @@ import Foundation
 public class KhipuVersion {
 
     /// The current version of the KhipuClientIOS SDK
-    public static let version = "2.16.3"
+    public static let version = "2.16.4"
 
     /// Private initializer to prevent instantiation
     private init() {}
